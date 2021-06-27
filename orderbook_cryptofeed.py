02a3f7ceac84178fc22165546a4c6276d72ea204
nunodesousa <nunodsousa@gmail.com>
2021-06-27 22:51:44 +0100

    Small Corrections

--- orderbook_cryptofeed.py
+++ orderbook_cryptofeed.py
@@ -1,17 +1,36 @@
+################################################################################
+# Very simple Cryptofeed example where we maintain a structured orderbook.
+# The major difference between this and and the demo.py presented as an example of the library
+# is the use of a orderbook defined in the main function.
+#
+# Nuno de Sousa
+# June 2021
+################################################################################
 
-from decimal import Decimal
 import functools as fct
 from cryptofeed import FeedHandler
 from cryptofeed.defines import BID, ASK, L2_BOOK
 from cryptofeed.exchanges import Kraken
+from datetime import datetime
+
+
+def filter_orderbook(orderbooks, book, symbol, depth = 10):
+    orderbook = {}
+    orderbook[BID] = dict(zip(list(book[BID].keys())[-depth:], list(book[BID].values()[-depth:])))
+    orderbook[ASK] = dict(zip(list(book[ASK].keys())[:depth], list(book[BID].values()[:depth])))
+    orderbooks[symbol] = orderbook
+    return orderbooks
 
 async def bookfunc(params, orderbooks, feed, symbol, book, timestamp, receipt_timestamp):
 
     print(5*"_*")
+    dt_object = datetime.fromtimestamp(timestamp)
+    print("datetime object: ", dt_object)
     print(f'Timestamp: {timestamp} Cryptofeed Receipt: {receipt_timestamp} Feed: {feed} Symbol: {symbol}'
           f' Book Bid Size is {len(book[BID])} Ask Size is {len(book[ASK])}')
     orderbooks = filter_orderbook(orderbooks, book, symbol, params['orderbook']['depth'])
     print(orderbooks)
+
 
 def main():
 
@@ -22,7 +41,7 @@ def main():
     orderbooks = {}
 
     f = FeedHandler(config=config)
-    f.add_feed(Kraken(checksum_validation=True, subscription={L2_BOOK: ['BTC-USD', 'ETH-USD']},
+    f.add_feed(Kraken(checksum_validation=True, subscription={L2_BOOK: ['BTC-USD', 'ETH-USD', 'LINK-USD', 'LTC-USD', 'ADA-USD']},
                       callbacks={L2_BOOK: fct.partial(bookfunc, params, orderbooks)})) # This way passes the orderbooks inside the callback
 
     # OnClick Run
